960a877fb2581baeec20470408e65876b39149d3
Fitriameliasitorus <fitamel86@gmail.com>
2024-12-12 10:01:38 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,8 +6,22 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+         System.out.print("Masukkan nilai N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
 
         /* Pekerjaan anda berakhir sini */
+        scanner.close();
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
